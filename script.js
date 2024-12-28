40bd5e2a9a5d13fa7f9746fe9ed07889884ba2f2
oybek003 <ojbekmuslimov1@gmail.com>
2024-12-28 22:33:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// if(things.includes('n')){
+    // let results = things.slice(0, 1, 2);
+    // console.log(results);
+// }else{
+    // let result = things.slice(3, 4, 5, 6)
+    // console.log(result);
+// }
+
+
+let things = [];
+things.splice(0, 1, "non", "banan", "mashina");
+things.join('' );
+console.log(things, "n harifi bor");
+
+
+let thing = [];
+thing.splice(0, 1, "asal", "olma", "aziza", "gul")
+things.join('' );
+console.log(thing, "n harifi yok")
